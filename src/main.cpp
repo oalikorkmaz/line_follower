a07d8abb37e9794bd3b6191d469761ffc2932f05
Onur Ali Korkmaz <onur.korkmaz@hotmail.com>
2024-05-12 03:24:17 +0300

    90 degree turn

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,34 @@
 #define Kp 0.01
 #define Ki 0.001
 #define Kd 0.002
-#define right_max_speed 114
-#define left_max_speed 115
-#define right_base_speed 85
-#define left_base_speed 86
 
-#define RIGHT_MOTOR_IN1 5
-#define RIGHT_MOTOR_IN2 6
+#define RIGHT_MOTOR_IN1 6
+#define RIGHT_MOTOR_IN2 5
 #define RIGHT_MOTOR_ENA 7
 
-#define LEFT_MOTOR_IN1 4
-#define LEFT_MOTOR_IN2 3
+#define LEFT_MOTOR_IN1 3
+#define LEFT_MOTOR_IN2 4
 #define LEFT_MOTOR_ENB 2
 
 #define THRESHOLD 1000
+
+// #define left_trig_pin 
+// #define left_echo_pin
+// #define mid_trig_pin
+// #define mid_echo_pin
+// #define right_trig_pin
+// #define right_echo_pin
 
 QTRSensors qtr;
 bool line = true; // true = white, false = black
 const uint8_t sensor_count = 8;
 unsigned int sensor_values[sensor_count];
 int last_error = 0;
+
 double set_point = 3500, input, output;
+double turn_input, turn_output, turn_set_point = 500;
 PID myPID(&input, &output, &set_point, Kp, Ki, Kd, DIRECT);
+PID turnPID(&turn_input, &turn_output, &turn_set_point, Kp, Ki, Kd, DIRECT);
 
 void motor_right(int duty){
     if(duty>=0){
@@ -51,19 +57,30 @@ void motor_left(int duty){
     analogWrite(LEFT_MOTOR_ENB, duty);
 }
 
+void motor_stop(){
+    digitalWrite(RIGHT_MOTOR_IN1, LOW);
+    digitalWrite(RIGHT_MOTOR_IN2, LOW);
+    digitalWrite(LEFT_MOTOR_IN1, LOW);
+    digitalWrite(LEFT_MOTOR_IN2, LOW);
+}
+
+// int distance(trig_pin, echo_pin){
+
+// }
 
 
 void surface(){
-    if(sensor_values[0] >= 700 && sensor_values[1] >= 700 && sensor_values[6] >= 700 && sensor_values[7] <= 700){
-        line = 1;
+    if(sensor_values[0] >= 700 && sensor_values[1] >= 700 && sensor_values[6] >= 700 && sensor_values[7] >= 700){
+        line = true;
     }
     if(sensor_values[0] <= 50 && sensor_values[1] <= 50 && sensor_values[6] <= 50 && sensor_values[7] <= 50){
-        line = 0;
+        line = false;
     }
 }
 
 void setup()
 {
+    Serial.begin(9600);
     pinMode(RIGHT_MOTOR_IN1, OUTPUT);
     pinMode(RIGHT_MOTOR_IN2, OUTPUT);
     pinMode(RIGHT_MOTOR_ENA, OUTPUT);
@@ -90,23 +107,55 @@ void setup()
 }
 
 void main_code(){
-    surface();
-    unsigned int position;
-    if (line) {
-        position = qtr.readLineWhite(sensor_values);
-    } else {
-        position = qtr.readLineBlack(sensor_values);
-    }
-
+    // surface();
+    // unsigned int position;
+    // if (line) {
+    //     position = qtr.readLineWhite(sensor_values);
+    // } else {
+    //     position = qtr.readLineBlack(sensor_values);
+    // }
+    unsigned int position = qtr.readLineBlack(sensor_values);
+    // turnPID.Compute();
+    // int turn_speed = turn_output;
+    
     input = position;
-    Serial.println("Position: " + position);
+    Serial.println("Position: " + String(position));
     myPID.Compute();
     motor_left(100 - output);
     motor_right(100 + output);
-}
 
+    if(position == 7000){
+        //Serial.println("icerde");
+        motor_right(100 + output);
+        delay(500);
+    }
+    
+    // if (turn_input < turn_set_point && input < set_point){
+    //     motor_right(100 + output);
+    // }else if(turn_input > turn_set_point && input > set_point){
+    //     motor_left(100 + output);
+    // }else{
+    //     motor_stop();
+
+    //     motor_right(100 + output);
+    //     delay(1000);
+    // }
+
+
+
+
+    // motor_left(100 - output);
+    // motor_right(100 + output);
+
+    
+
+    // if (sensor_values == 0){
+    //     motor_right(70 + output);
+    // }
+}
 void loop()
 {   
+   
     main_code();
 }
 
